498b890154728b1213bca263ab52f4a9befd57b8
Gevand <gev-and@msn.com>
2023-04-23 21:36:38 -0700

    Ch4 SBS

--- ch-4-partition-data.py
+++ ch-4-partition-data.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
@@ -41,3 +43,43 @@ X_train_std = stdsc.fit_transform(X_train)
 X_test_std = stdsc.transform(X_test)
 print('STD value of ', X_train_std[0, 0], ' vs ', 'Norm value of ', X_train_norm[0, 0],
       ' vs real value of ', X_train[0, 0])
+
+lr = LogisticRegression(penalty='l1', C=1.0,
+                        solver='liblinear', multi_class='ovr')  # ovr means one verses rest
+lr.fit(X_train_std, y_train)
+print('Training accuracy:', lr.score(X_train_std, y_train))
+print('Test accuracy:', lr.score(X_test_std, y_test))
+
+print(lr.intercept_)
+print(lr.coef_)
+
+fig = plt.figure()
+ax = plt.subplot(111)
+colors = ['blue', 'green', 'red', 'cyan',
+          'magenta', 'yellow', 'black',
+          'pink', 'lightgreen', 'lightblue',
+          'gray', 'indigo', 'orange']
+
+weights, params = [], []
+for c in np.arange(-4., 6.):
+    lr = LogisticRegression(
+        penalty='l1', C=10.**c, solver='liblinear', multi_class='ovr', random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+weights = np.array(weights)
+for column, color in zip(range(weights.shape[1]), colors):
+    plt.plot(params, weights[:, column],
+             label=df_wine.columns[column + 1],
+             color=color)
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+plt.xlim([10**(-5), 10**5])
+plt.ylabel('Weight coefficient')
+plt.xlabel('C (inverse regularization strength)')
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center',
+            bbox_to_anchor=(1.38, 1.03),
+            ncol=1, fancybox=True)
+plt.show()
---
+++ ch-4-sbs.py
@@ -0,0 +1,106 @@
+
+import matplotlib.pyplot as plt
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from itertools import combinations
+import numpy as np
+from sklearn.base import clone
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+# This is a dimensionality reduction algorithm: sequential backward selection
+
+# 1 Initialize the algorithm with k = d, where d is the dimensionality of the full feature space, Xd.
+# 2 Determine the feature, x', that maximizes the criterion: x' = argmax J(Xk – x), where 𝒙 element of Xk
+# 3 Remove the feature, x', from the feature set: Xk–1 = Xk – x'; k = k – 1.
+# 4 Terminate if k equals the number of desired features; otherwise, go to step 2
+
+
+class SBS:
+    def __init__(self, estimator, k_features, scoring=accuracy_score, test_size=.25, random_state=1) -> None:
+        self.scoring = scoring
+        self.estimator = clone(estimator)
+        self.k_features = k_features
+        self.test_size = test_size
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=self.test_size,
+                                                            random_state=self.random_state)
+        dim = X_train.shape[1]
+        self.indeces_ = tuple(range(dim))
+        self.subsets_ = [self.indeces_]
+        score = self._calc_score(
+            X_train, y_train, X_test, y_test, self.indeces_)
+        self.scores_ = [score]
+
+        while dim > self.k_features:
+            scores = []
+            subsets = []
+
+            for p in combinations(self.indeces_, r=dim - 1):
+                score = self._calc_score(X_train, y_train, X_test, y_test, p)
+                scores.append(score)
+                subsets.append(p)
+
+            best = np.argmax(scores)
+            self.indices_ = subsets[best]
+            self.subsets_.append(self.indices_)
+            dim -= 1
+
+            self.scores_.append(scores[best])
+        self.k_scores_ = self.scores_[-1]
+        return self
+
+    def transofrm(self, X):
+        return X[:, self.indices_]
+
+    def _calc_score(self, X_train, y_train, X_test, y_test, indices):
+        self.estimator.fit(X_train[:, indices], y_train)
+        y_pred = self.estimator.predict(X_test[:, indices])
+        score = self.scoring(y_test, y_pred)
+        return score
+
+
+df_wine = pd.read_csv(
+    'https://archive.ics.uci.edu/ml/'
+    'machine-learning-databases/wine/wine.data',
+    header=None
+)
+
+df_wine.columns = ['Class label', 'Alcohol',
+                   'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium',
+                   'Total phenols', 'Flavanoids',
+                   'Nonflavanoid phenols',
+                   'Proanthocyanins',
+                   'Color intensity', 'Hue',
+                   'OD280/OD315 of diluted wines',
+                   'Proline']
+
+print(df_wine.head())
+print('Class lables', np.unique(df_wine['Class label']))
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=.3, random_state=0, stratify=y)
+print(X_train[0:5, :], y_train[0:5])
+
+
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.transform(X_test)
+
+knn = KNeighborsClassifier(n_neighbors=5)
+sbs = SBS(knn, k_features=1)
+sbs.fit(X_train_std, y_train)
+
+k_feat = [len(k) for k in sbs.subsets_]
+plt.plot(k_feat, sbs.scores_, marker='o')
+plt.ylim([0.7, 1.02])
+plt.ylabel('Accuracy')
+plt.xlabel('Number of features')
+plt.grid()
+plt.tight_layout()
+plt.show()
