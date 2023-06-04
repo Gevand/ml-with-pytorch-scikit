d7b6d4e0ffcc38aba692ca5a931400ba38d432f7
Gevand <gev-and@msn.com>
2023-06-03 18:52:47 -0700

    Created my version of the rating classifier

--- excersize-movie-recommendation/base_tree_model.py
+++ excersize-movie-recommendation/base_tree_model.py
@@ -1,6 +1,9 @@
 from matplotlib import pyplot as plt
 import pandas as pd
+from sklearn.calibration import LabelEncoder
+from sklearn.metrics import auc, precision_score, roc_curve
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 genre_cols = [
     "genre_unknown", "Action", "Adventure", "Animation", "Children", "Comedy",
@@ -30,11 +33,11 @@ print('-' * 10)
 print(df_ratings.head())
 print('-' * 10)
 
-df_users[['age']].hist()
+# df_users[['age']].hist()
 # plt.show()
-df_users[['sex', 'occupation']].value_counts().plot(kind='bar')
+# df_users[['sex', 'occupation']].value_counts().plot(kind='bar')
 # plt.show()
-df_ratings[['rating']].hist()
+# df_ratings[['rating']].hist()
 # plt.show()
 
 # figure out the movie ratings
@@ -56,3 +59,33 @@ print(df_movies_ratings[['title', 'rating count', 'rating mean']].sort_values(
 print('Top 10 highest rated movies with atleast 20 ratings: ')
 print(df_movies_ratings[['title', 'rating count', 'rating mean']
                         ].loc[df_movies_ratings['rating count'] > 20].sort_values('rating mean', ascending=False).head(10))
+
+df_dataset = df_ratings.merge(
+    df_users, on='user_id').merge(df_movies, on='movie_id')
+print(df_dataset.head(), df_dataset.shape)
+
+print(df_dataset.columns.values)
+X_columns = ['age', 'sex', 'occupation',
+             'zip_code', 'Action', 'Adventure', 'Animation', 'Children', 'Comedy',
+             'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'Musical',
+             'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
+
+y_columns = ['rating']
+X = pd.get_dummies(df_dataset[X_columns]).values
+y = df_dataset[y_columns].values.flatten()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=1, stratify=y)
+
+forest_clf = RandomForestClassifier(
+    n_estimators=25, random_state=1, n_jobs=2)
+forest_clf.fit(X_train, y_train)
+
+y_pred = forest_clf.predict(X_train)
+
+for i in range(20):
+    print(f'True: {y_train[i]} vs Predicted: {y_pred[i]}')
+
+prec_score = precision_score(y_train, y_pred, average='micro')
+
+print('Precision score ', prec_score)
