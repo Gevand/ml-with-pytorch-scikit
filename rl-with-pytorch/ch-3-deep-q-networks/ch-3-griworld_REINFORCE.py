082a9cc56aee4667483652fcd440297d91745ee9
Gevand <gev-and@msn.com>
2024-06-02 13:48:35 -0700

    Set up REINFORCE for ch3 from ch4

---
+++ rl-with-pytorch/ch-3-deep-q-networks/ch-3-griworld_REINFORCE.py
@@ -0,0 +1,17 @@
+from collections import deque
+import math
+from gridworld import Gridworld
+import torch
+import numpy as np
+import random
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+import copy
+
+
+np.random.seed(42)
+random.seed(42)
+torch.manual_seed(42)
+
+
+print('TEST')
