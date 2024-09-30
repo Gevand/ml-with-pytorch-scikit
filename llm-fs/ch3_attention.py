e6df7c7e777f30b17f765b13cee38e7d26885dd4
Gevand <gev-and@msn.com>
2024-09-30 11:11:58 -0700

    Ch3

---
+++ llm-fs/ch3_attention.py
@@ -0,0 +1,148 @@
+
+import torch.nn as nn
+import torch
+print("Starting on ch 3")
+
+
+def softmax_naive(x):
+    return torch.exp(x) / torch.exp(x).sum(dim=0)
+
+
+inputs = torch.tensor(
+    [[0.43, 0.15, 0.89],  # Your
+     [0.55, 0.87, 0.66],  # journey
+     [0.57, 0.85, 0.64],  # starts
+     [0.22, 0.58, 0.33],  # with
+     [0.77, 0.25, 0.10],  # one
+     [0.05, 0.80, 0.55]]  # step
+)
+
+print(inputs)
+query = inputs[1]  # journey
+attn_scores_2 = torch.empty(inputs.shape[0])
+for i, x_i in enumerate(inputs):
+    attn_scores_2[i] = torch.dot(x_i, query)
+print(attn_scores_2)
+
+# normalize
+attn_weights_2_tmp = attn_scores_2 / attn_scores_2.sum()
+print("Attention weights:", attn_weights_2_tmp)
+print("Sum:", attn_weights_2_tmp.sum())
+
+# normalize with softmax
+attn_weights_2_tmp = softmax_naive(attn_scores_2)
+print("Attention weights:", attn_weights_2_tmp)
+print("Sum:", attn_weights_2_tmp.sum())
+
+# normalize with torch softmax to increase numerical stability
+attn_weights_2 = torch.softmax(attn_scores_2, dim=0)
+print("Attention weights:", attn_weights_2)
+print("Sum:", attn_weights_2.sum())
+
+
+query = inputs[1]  # journeyh
+context_vec_2 = torch.zeros(query.shape)
+for i, x_i in enumerate(inputs):
+    context_vec_2 += attn_weights_2[i] * x_i
+print(context_vec_2)
+
+# for loop dot product for all inputs
+attn_scores = torch.empty(6, 6)
+for i, x_i in enumerate(inputs):
+    for j, x_j in enumerate(inputs):
+        attn_scores[i, j] = torch.dot(x_i, x_j)
+print(attn_scores)
+
+# torch dot product that's faster
+attn_scores = inputs @ inputs.T
+print(attn_scores)
+
+attn_weights = torch.softmax(attn_scores, dim=1)
+print(attn_weights)
+
+row_2_sum = sum(attn_weights[1])
+print(row_2_sum)
+print("All row sums:", attn_weights.sum(dim=1))
+
+all_context_vecs = attn_weights @ inputs
+print(all_context_vecs)
+
+x_2 = inputs[1]
+d_in = inputs.shape[1]
+d_out = 2
+
+w_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+w_key = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+w_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+
+query_2 = x_2 @ w_query
+key_2 = x_2 @ w_key
+value_2 = x_2 @ w_value
+print(query_2)
+
+keys = inputs @ w_key
+values = inputs @ w_value
+print("keys.shape:", keys.shape)
+print("values.shape:", values.shape)
+
+keys_2 = keys[1]
+attn_score_22 = query_2.dot(keys_2)
+print(attn_score_22)
+
+attn_scores_2 = query_2 @ keys.T
+print(attn_scores_2)
+
+d_k = keys.shape[-1]
+attn_weights_2 = torch.softmax(attn_scores_2 / d_k**0.5, dim=-1)
+print(attn_weights_2)
+
+context_vec_2 = attn_weights_2 @ values
+print(context_vec_2)
+
+
+class SelfAttention_v1(nn.Module):
+    def __init__(self, d_in, d_out) -> None:
+        super().__init__()
+        self.d_out = d_out
+        self.w_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.w_key = nn.Parameter(torch.rand(d_in, d_out))
+        self.w_value = nn.Parameter(torch.rand(d_in, d_out))
+
+    def forward(self, x):
+        keys = x @ self.w_key
+        queries = x @ self.w_query
+        values = x @ self.w_value
+
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+print(sa_v1(inputs))
+
+
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))
