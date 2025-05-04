5be26f87f315c648dc632feda4c18f5b5e32f455
Gevand <gev-and@msn.com>
2025-05-04 11:48:48 -0700

    Picked up ch3 again

--- llm-fs/ch3_attention.py
+++ llm-fs/ch3_attention.py
@@ -184,6 +184,79 @@ print(batch.shape)
 class CausalAttention(nn.Module):
     def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
         super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_Key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_Value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer('mask', torch.triu(
+            torch.ones(context_length, context_length), diagonal=1))
 
     def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        keys = self.W_Key(x)
+        values = self.W_Value(x)
+
+        attn_scores = queries @ keys.transpose(1, 2)
+        attn_scores.masked_fill_(
+            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1] ** .05, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+torch.manual_seed(123)
+context_length = batch.shape[1]
+ca = CausalAttention(d_in, d_out, context_length, 0.0)
+
+context_vecs = ca(batch)
+
+print(context_vecs)
+print(f"context_vecs.shape {context_vecs.shape}")
+
+
+class MultiHeadAttenionWrapper(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.heads = nn.ModuleList([CausalAttention(
+            d_in, d_out, context_length, dropout, qkv_bias) for _ in range(num_heads)])
+
+    def forward(self, x):
+        result = []
+        for head in self.heads:
+            result.append(head.forward(x))
+        return torch.cat(result, dim=-1)
+
+
+torch.manual_seed(123)
+context_length = batch.shape[1]
+d_in, d_out = 3, 2
+mha = MultiHeadAttenionWrapper(d_in, d_out, context_length, 0.0, 2, False)
+context_vecs = mha(batch)
+print(context_vecs)
+print(f"context_vecs.shape {context_vecs.shape}")
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert (d_out % num_heads == 0), "d_out must be divisible by b"
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out
+
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+        self.out_proj = nn.Linear(d_out, d_out)
+
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer('mask', torch.triu(
+            torch.ones(context_length, context_length), diagonal=1))
+
+    def foward(self, x):
         pass
